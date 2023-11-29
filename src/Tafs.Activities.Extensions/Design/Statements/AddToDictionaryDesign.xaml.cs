de8375aebd833dae8184ff1823a3b54693a03e42
Devin Duanne <dduanne@tafs.com>
2023-11-29 13:33:18 -0600

    Update Tafs.Activities.Extensions

--- src/Tafs.Activities.Extensions/Design/Statements/AddToDictionaryDesign.xaml.cs
+++ src/Tafs.Activities.Extensions/Design/Statements/AddToDictionaryDesign.xaml.cs
@@ -20,6 +20,13 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+using System;
+using System.Activities.Presentation.Metadata;
+using System.Collections.Generic;
+using System.Windows;
+using Tafs.Activities.Extensions.Statements;
+using DependencyProp = System.Windows.DependencyProperty;
+
 namespace Tafs.Activities.Extensions.Design.Statements
 {
     /// <summary>
@@ -27,6 +34,49 @@ namespace Tafs.Activities.Extensions.Design.Statements
     /// </summary>
     public partial class AddToDictionaryDesign
     {
+        /// <summary>
+        /// Gets the binding property for the dictionary type.
+        /// </summary>
+        public static readonly DependencyProp DictionaryTypeProperty = DependencyProp.Register(nameof(DictionaryType), typeof(Type), typeof(AddToDictionaryDesign), new UIPropertyMetadata(null));
+
+        /// <summary>
+        /// Gets the binding property for the key type.
+        /// </summary>
+        public static readonly DependencyProp KeyTypeProperty = DependencyProp.Register(nameof(KeyType), typeof(Type), typeof(AddToDictionaryDesign), new UIPropertyMetadata(null));
+
+        /// <summary>
+        /// Gets the binding property for the value type.
+        /// </summary>
+        public static readonly DependencyProp ValueTypeProperty = DependencyProp.Register(nameof(ValueType), typeof(Type), typeof(AddToDictionaryDesign), new UIPropertyMetadata(null));
+
+        private Type DictionaryType
+        {
+            get => (Type)GetValue(DictionaryTypeProperty);
+            set => SetValue(DictionaryTypeProperty, value);
+        }
+
+        private Type KeyType
+        {
+            get => (Type)GetValue(KeyTypeProperty);
+            set => SetValue(KeyTypeProperty, value);
+        }
+
+        private Type ValueType
+        {
+            get => (Type)GetValue(ValueTypeProperty);
+            set => SetValue(ValueTypeProperty, value);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnModelItemChanged(object newItem)
+        {
+            base.OnModelItemChanged(newItem);
+            Type[] genericArguments = ModelItem.ItemType.GetGenericArguments();
+            KeyType = genericArguments[0];
+            ValueType = genericArguments[1];
+            DictionaryType = typeof(Dictionary<,>).MakeGenericType(genericArguments);
+        }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="AddToDictionaryDesign"/> class.
         /// </summary>
