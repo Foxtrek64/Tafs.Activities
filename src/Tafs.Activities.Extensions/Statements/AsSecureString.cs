b9c9731b31e1319ef843aafba8d73f80bab400c8
Devin Duanne <dduanne@tafs.com>
2023-12-04 09:35:30 -0600

    Don't pack some packages.

--- src/Tafs.Activities.Extensions/Statements/AsSecureString.cs
+++ src/Tafs.Activities.Extensions/Statements/AsSecureString.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿//
+//  AsSecureString.cs
+//
+//  Author:
+//       Devin Duanne <dduanne@tafs.com>
+//
+//  Copyright (c) TAFS, LLC.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
 using System.Activities;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,8 +36,8 @@ namespace Tafs.Activities.Extensions.Statements
     /// Converts the provided string into a SecureString.
     /// </summary>
     [DisplayName("String As SecureString")]
-    [Description("Provides an activity to convert a string variable to a SecureString.")]
-    public sealed class AsSecureString : CodeActivity<SecureString>
+    [Description("Convert a string variable to a SecureString. If the input string is null or empty, a null SecureString is returned.")]
+    public sealed class AsSecureString : CodeActivity<SecureString?>
     {
         /// <summary>
         /// Gets or sets the input string.
@@ -26,15 +48,11 @@ namespace Tafs.Activities.Extensions.Statements
         public InArgument<string> Input { get; set; } = new();
 
         /// <inheritdoc/>
-        protected override SecureString Execute(CodeActivityContext context)
+        protected override SecureString? Execute(CodeActivityContext context)
         {
             var input = Input.Get(context);
 
-            if (string.IsNullOrEmpty(input))
-            {
-                return default;
-            }
-            return input.AsSecureString();
+            return string.IsNullOrEmpty(input) ? default : input.AsSecureString();
         }
     }
 }
